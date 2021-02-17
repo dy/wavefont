2e9247d00b5b2334e8f05bd1bb2ec6ecf7d84688
dy <df.creative@gmail.com>
2021-02-17 10:39:05 -0500

    Add 1,100 masters

--- build-values.sh
+++ build-values.sh
@@ -1,24 +1,28 @@
+#!/usr/bin/env bash
+
 offset=256
+
+# FIXME: fallback to default or show error
+width=$1
 
 for((i=0;i<=100;i++))
   do
     charCode=$(expr $offset + $i)
     hex=$(printf '0%x' $charCode)
-    # touch "$PWD/wavefont.ufo/glyphs/${i}.glif"
-    # echo "${hex}" > "./glyphs/${i}.glif"
+    echo "Creating $i.glif"
     echo '<?xml version="1.0" encoding="UTF-8"?>
 <glyph name="_" format="2">
-  <advance width="10"/>
-  <unicode hex="'${hex}'"/>
+  <advance width="'$width'"/>
+  <unicode hex="'$hex'"/>
   <outline>
     <contour>
-      <point x="0" y="'${i}'" type="line"/>
-      <point x="10" y="'${i}'" type="line"/>
-      <point x="10" y="0" type="line"/>
+      <point x="0" y="'$i'" type="line"/>
+      <point x="'$width'" y="'$i'" type="line"/>
+      <point x="'$width'" y="0" type="line"/>
       <point x="0" y="0" type="line"/>
     </contour>
   </outline>
   <anchor name="entry" x="0" y="0"/>
   <anchor name="exit" x="10" y="0"/>
-</glyph>' > "$PWD/wavefont.ufo/glyphs/${i}.glif"
+</glyph>' > "$PWD/masters/$width.ufo/glyphs/$i.glif"
   done
