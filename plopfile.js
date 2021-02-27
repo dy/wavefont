ef67c687da5ef270d85286b408009d57e466c6a3
dy <df.creative@gmail.com>
2021-02-26 21:18:49 -0500

    Create caps-based radius

--- plopfile.js
+++ plopfile.js
@@ -12,7 +12,6 @@ const values100 = Array.from({length: 129}).map((v,i)=>({value: i, code: i+0x010
 // 0xE000-F900                            ~6400
 // ^_ - relative offset?
 
-
 module.exports = function (plop) {
   const maxWidth = 20
   const maxValue = 100
@@ -35,29 +34,30 @@ module.exports = function (plop) {
       ...master({values, maxValue, maxWidth, align: 0, width: maxWidth, radius: 50}),
 
       // write clipping glyphs rules
-      {type: "modify", path:"masters/wavefont.designspace", pattern:/<rules>([^]*?)<\/rules>/i, template: `<rules>${
-        values.filter(({clip})=>clip).map(({value}) => `
-        <rule name="clip">
-            <conditionset><condition minimum="${value}" maximum="${maxWidth}" name="width" /></conditionset>
-            <sub name="_${value}" with="_${value}.clip"/>
-        </rule>`).join('')
-      }</rules>`},
+      // {type: "modify", path:"masters/wavefont.designspace", pattern:/<rules>([^]*?)<\/rules>/i, template: `<rules>${
+      //   values.filter(({clip})=>clip).map(({value}) => `
+      //   <rule name="clip">
+      //       <conditionset><condition minimum="${value}" maximum="${maxWidth}" name="width" /></conditionset>
+      //       <sub name="_${value}" with="_${value}.clip"/>
+      //   </rule>`).join('')
+      // }</rules>`},
 
       // write GlyphsOrderAndAlias
-      {type: "modify", path:"masters/GlyphOrderAndAliasDB", pattern:/#values[^]*#\/values/i, template: `#values
-      ${
+      {type: "modify", path:"masters/GlyphOrderAndAliasDB", pattern:/#values[^]*#\/values/i, template: `#values\n${
         values.map(({code, value}) => `_${value}\t_${value}\tuni${hex(code)}`).join('\n')
-      }
-      ${
-        values.filter(({clip}) => clip).map(({value}) => `_${value}.clip\t_${value}.clip`).join('\n')
-      }
-      #/values`}
+      }\n${
+        ``// values.filter(({clip}) => clip).map(({value}) => `_${value}.clip\t_${value}.clip`).join('\n')
+      }\n#/values`}
     ]
 	});
 };
 
 // create actions to build one master file
 function master({values, maxValue, maxWidth, align, width, radius}){
+  const R = radius * .01 * width,
+        // bezier curve shift to approximate border-radius
+        Rc = R * (1 - .55),
+        capH = width*.5;
   return [
     // populate ufo skeleton
     {
@@ -83,63 +83,84 @@ function master({values, maxValue, maxWidth, align, width, radius}){
             </location>
         </source></sources>`
     },
+    // top/bottom caps
+    {
+      verbose: false,
+      force: true,
+      type: 'add',
+      path: `masters/${width}_${align}_${radius}.ufo/glyphs/cap.top.glif`,
+      template: `<?xml version="1.0" encoding="UTF-8"?>
+        <glyph name="cap.top" format="2"><advance width="${width}"/><outline><contour>
+          <point x="0" y="0" type="line"/>
+          <point x="0" y="${(capH - R)}" type="line"/>
+          <point x="0" y="${trim((capH - Rc))}"/>
+          <point x="${Rc}" y="${capH}"/>
+          <point x="${R}" y="${capH}" type="curve" smooth="yes"/>
+          <point x="${width - R}" y="${capH}" type="line"/>
+          <point x="${width - Rc}" y="${capH}"/>
+          <point x="${width}" y="${trim((capH - Rc))}"/>
+          <point x="${width}" y="${(capH - R)}" type="curve" smooth="yes"/>
+          <point x="${width}" y="0" type="line"/>
+        </contour></outline></glyph>`
+    },
+    {
+      verbose: false,
+      force: true,
+      type: 'add',
+      path: `masters/${width}_${align}_${radius}.ufo/glyphs/cap.bottom.glif`,
+      template: `<?xml version="1.0" encoding="UTF-8"?>
+        <glyph name="cap.bottom" format="2"><advance width="${width}"/><outline><contour>
+          <point x="${width}" y="0" type="line"/>
+          <point x="${width}" y="${-capH + R}" type="line"/>
+          <point x="${width}" y="${trim(-capH + Rc)}"/>
+          <point x="${width - Rc}" y="${-capH}"/>
+          <point x="${width - R}" y="${-capH}" type="curve" smooth="yes"/>
+          <point x="${R}" y="${-capH}" type="line"/>
+          <point x="${Rc}" y="${-capH}"/>
+          <point x="${0}" y="${trim(-capH + Rc)}"/>
+          <point x="${0}" y="${-capH + R}" type="curve" smooth="yes"/>
+          <point x="0" y="0" type="line"/>
+        </contour></outline></glyph>`
+    },
+    // values
     ...values.map(({code, value}) => ({
       verbose: false,
       force: true,
       type: 'add',
       path: `masters/${width}_${align}_${radius}.ufo/glyphs/${value}.glif`,
-      template: glyph({value, width, align, code, maxValue, radius: (radius && 1) * width*.5})
+      template: glyph({value, width, align, code, maxValue, maxWidth})
     })),
     // substitute glyphs lower than max width to compensate wrong interpolation on width clipping
     // the logic: big widths would have big radius, but since it's limited to value, we interpolate between wrong 1 width and max width
-    ...values.filter(({clip}) => clip).map(({value}) => ({
-      verbose: false,
-      force: true,
-      type: 'add',
-      path: `masters/${width}_${align}_${radius}.ufo/glyphs/${value}.clip.glif`,
-      template: glyph({value, width, align, maxValue, radius: (radius && 1) * value*.5})
-    }))
+    // ...values.filter(({clip}) => clip).map(({value}) => ({
+    //   verbose: false,
+    //   force: true,
+    //   type: 'add',
+    //   path: `masters/${width}_${align}_${radius}.ufo/glyphs/${value}.clip.glif`,
+    //   template: glyph({value, width, align, maxValue})
+    // }))
   ]
 }
 
-
-const glyph = ({value, width, align, code, maxValue, radius}) => {
+const glyph = ({value, width, align, code, maxValue, maxWidth}) => {
   const baseline=align * maxValue,
-        R=radius,
-        // bezier curve shift to approximate border-radius
-        Rc = R * (1 - .55),
+        R=width*.5,
         // alignment constant shift
         Ca = (maxValue - value) * align
 
   return `<?xml version="1.0" encoding="UTF-8"?>
-<glyph name="_" format="2">
+<glyph name="_${value}" format="2">
   <advance width="${width}"/>
   ${code ? `<unicode hex="${hex(code)}"/>` : ``}
   ${value && `<outline>
-    <contour>${
-      `
-      <point x="0" y="${trim(value-R + Ca)}" type="line" smooth="yes"/>
-      <point x="0" y="${trim(value-Rc + Ca)}"/>
-
-      <point x="${trim(Rc)}" y="${trim(value + Ca)}"/>
-      <point x="${R}" y="${trim(value + Ca)}" type="curve" smooth="yes"/>
-      <point x="${width-R}" y="${trim(value + Ca)}" type="line" smooth="yes"/>
-      <point x="${trim(width-Rc)}" y="${trim(value + Ca)}"/>
-
-      <point x="${width}" y="${trim(value-Rc + Ca)}"/>
-      <point x="${width}" y="${trim(value-R + Ca)}" type="curve" smooth="yes"/>
-      <point x="${width}" y="${trim(R + Ca)}" type="line" smooth="yes"/>
-      <point x="${width}" y="${trim(Rc + Ca)}"/>
-
-      <point x="${trim(width-Rc)}" y="${trim(Ca)}"/>
-      <point x="${width-R}" y="${trim(Ca)}" type="curve" smooth="yes"/>
-      <point x="${R}" y="${trim(Ca)}" type="line" smooth="yes"/>
-      <point x="${trim(Rc)}" y="${trim(Ca)}"/>
-
-      <point x="0" y="${trim(Rc + Ca)}"/>
-      <point x="0" y="${trim(R + Ca)}" type="curve" smooth="yes"/>
-      `
-    }</contour>
+    <component base="cap.top" xOffset="0" yOffset="${value + Ca - R}" />
+    <component base="cap.bottom" xOffset="0" yOffset="${Ca + R}" />
+    <contour>
+      <point x="0" y="${Ca + R}" type="line"/>
+      <point x="0" y="${value + Ca - R}" type="line"/>
+      <point x="${width}" y="${value + Ca - R}" type="line"/>
+      <point x="${width}" y="${Ca + R}" type="line"/>
+    </contour>
   </outline>`}
   <anchor name="entry" x="0" y="${trim(baseline)}"/>
   <anchor name="exit" x="${width}" y="${trim(baseline)}"/>
