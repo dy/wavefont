fe5078b13f88c3523b6d6910c8f64583247428bc
Dmitry Iv <df.creative@gmail.com>
2023-03-22 09:15:52 -0400

    Rename axis to y alignment

--- plopfile.js
+++ plopfile.js
@@ -51,9 +51,9 @@ function alias(value, char) {
 
 // axes definition, per https://github.com/dy/wavefont/issues/42
 const AXES = {
-  roundness: {tag: 'ROND', min: 0, max: 100, default: 0},
-  weight: {tag: 'wght', min: 1, max: 400, default: 400},
-  align: {tag: 'YALN', min: 0, max: 1, default: 0}
+  roundness: {name: 'Roundness', tag: 'ROND', min: 0, max: 100, default: 0},
+  weight: {name: 'Weight', tag: 'wght', min: 1, max: 400, default: 400},
+  align: {name: 'Y Alignment', tag: 'YALN', min: 0, max: 1, default: 0}
 }
 
 // create masters
@@ -100,6 +100,9 @@ module.exports = function (plop) {
 
       // int 12.3 → 12
       plop.setHelper('int', v => v.toFixed(0));
+
+      // align -> Y Alignment
+      plop.setHelper('axisName', v => AXES[v].name);
 
       // {{#times N}}{{@index}}{{/times}}
       plop.setHelper('times', function(n, block) {
